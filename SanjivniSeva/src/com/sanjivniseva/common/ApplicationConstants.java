6ab42e502acda0eeb7e2ec2febff22f614240554
jsingh312@gmail.com <jsingh312@gmail.com>
2016-03-05 20:53:28 +0530

    CHECKING

--- SanjivniSeva/src/com/sanjivniseva/common/ApplicationConstants.java
+++ SanjivniSeva/src/com/sanjivniseva/common/ApplicationConstants.java
@@ -3,4 +3,5 @@ package com.sanjivniseva.common;
 public class ApplicationConstants {
 	public static final String METHOD_START = "Method Start";
 	public static final String METHOD_END = "Method End";
+	public static final String METHOD_CHECK = "Method End";
 }
